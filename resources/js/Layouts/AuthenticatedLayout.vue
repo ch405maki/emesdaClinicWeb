71cb1613eb3e117740c29b9aec5cca57bb932aa2
ch405maki <markmanuel0317@gmail.com>
2024-09-29 20:39:19 +0800

    Working on user and availability

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -131,10 +131,10 @@
               <div class="pt-10">
                 <p class="px-3 text-sm font-medium text-gray-500" id="communities-headline">Settings</p>
                 <div class="mt-3 space-y-2" aria-labelledby="communities-headline">
-                    <a href="#" class="group flex items-center rounded-md px-3 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 hover:text-gray-900">
+                    <a :href="route('dentist.availability')" class="group flex items-center rounded-md px-3 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 hover:text-gray-900">
                       <span class="truncate">Availability</span>
                     </a>
-                    <a href="#" class="group flex items-center rounded-md px-3 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 hover:text-gray-900">
+                    <a :href="route('staff')" class="group flex items-center rounded-md px-3 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 hover:text-gray-900">
                       <span class="truncate">Manage Staff</span>
                     </a>
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@ use App\Http\Controllers\AppointmentController;
 use App\Http\Controllers\DiagnosticController;
 use App\Http\Controllers\ReportController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\StaffController;
+use App\Http\Controllers\DentistAvailabilityController;
 
 
 /*
@@ -67,6 +69,15 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/reports', [ReportController::class, 'index'])->name('reports.index');
     Route::get('/reports/{id}', [ReportController::class, 'show'])->name('reports.show');
     Route::get('/reports/{id}/export-pdf', [ReportController::class, 'exportPdf'])->name('reports.export-pdf');
+
+    //Staff management
+    Route::get('/staff', [StaffController::class, 'index'])->name('staff');
+    Route::post('/staff', [StaffController::class, 'store'])->name('staff.store');
+    Route::delete('/staff/{id}', [StaffController::class, 'destroy'])->name('staff.destroy');
+
+    Route::get('/dentist/availability', [DentistAvailabilityController::class, 'index'])->name('dentist.availability');
+    Route::post('/dentist/availability', [DentistAvailabilityController::class, 'store'])->name('dentist.availability.store');
+    Route::delete('/dentist/availability/{id}', [DentistAvailabilityController::class, 'destroy'])->name('availability.destroy');
 });
 
 Route::get('/download-apk', function () {
